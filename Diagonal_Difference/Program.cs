2eb19683bcb9263e66847ebd25e2f67f080a9762
Ashansa Mahra <mahraashansa1997@gmail.com>
2024-01-03 19:18:54 +0530

    Add files via upload

---
+++ Diagonal_Difference/Program.cs
@@ -0,0 +1,41 @@
+﻿class Result
+{
+
+    public static int DiagonalDifference(List<List<int>> arr)
+    {
+        int length = arr.Count;
+        var leftToRightDiagonalSum = 0;
+        var rightToLeftDiagonalSum = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            leftToRightDiagonalSum += arr[i][i];
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            rightToLeftDiagonalSum += arr[i][length - 1 - i];
+        }
+        return Math.Abs(leftToRightDiagonalSum - rightToLeftDiagonalSum);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.DiagonalDifference(arr);
+
+        Console.WriteLine(result);
+    }
+}
